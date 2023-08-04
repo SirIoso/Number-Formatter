815b181a35e341132b03cc8aa74a1717f6cda0c2
Ilse-Swanepoel <117873875+Ilse-Swanepoel@users.noreply.github.com>
2023-08-04 15:27:09 +1200

    Update scriptList.js

--- scriptList.js
+++ scriptList.js
@@ -8,31 +8,17 @@ function convertToList(input) {
     // Loop through the itemsArray and format each item
     itemsArray.forEach((item, index) => {
         // Add the item to the resultList with the desired format
-        resultList += `'${item}',\n`;
+        resultList += `'${item}', `;
     });
 
     return resultList;
 }
 
 function formatList() {
-    const inputList = document.getElementById('numberList').value;
-    const formattedList = convertToList(inputList);
-    // const formattedTable = document.getElementById('formattedTable');
+    const inputItemList = document.getElementById('itemList').value;
+    const formattedItemList = convertToList(inputItemList);
     const formattedListDiv = document.getElementById('formattedList');
 
-    // Clear existing table rows
-    formattedTable.innerHTML = '';
-
-    // Split the formattedList into an array of lines
-    const lines = formattedList.trim().split('\n');
-
-    // Create a new row for each line and add it to the table
-    lines.forEach((line) => {
-        const newRow = formattedTable.insertRow();
-        const newCell = newRow.insertCell();
-        newCell.textContent = line;
-    });
-
     // Update the formatted list in the HTML
-    formattedListDiv.textContent = formattedList;
+    formattedListDiv.textContent = formattedItemList;
 }
