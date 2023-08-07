b93aca0a1c69a1d6e714769cb93d70a7937188d1
Ilse-Swanepoel <117873875+Ilse-Swanepoel@users.noreply.github.com>
2023-08-07 16:13:51 +1200

    add copyText function

--- range.js
+++ range.js
@@ -52,3 +52,18 @@ function formatPort() {
     const lineCount = tableRows.split('<tr>').length - 1;
     counterElement.textContent = `Total lines: ${lineCount}`;
 }
+
+function copyText() {
+  // Get the text field
+  var copyText = document.getElementById("myInput");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
+}
