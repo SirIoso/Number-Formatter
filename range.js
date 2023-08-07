8836a5152d779a99c564bcb359794ea8d7ad8964
Ilse-Swanepoel <117873875+Ilse-Swanepoel@users.noreply.github.com>
2023-08-07 16:24:01 +1200

    Update range.js

--- range.js
+++ range.js
@@ -59,7 +59,6 @@ function copyText() {
 
   // Select the text field
   copyText.select();
-  copyText.setSelectionRange(0, 99999); // For mobile devices
 
    // Copy the text inside the text field
   navigator.clipboard.writeText(copyText.value);
