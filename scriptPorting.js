9e111237b272c438a8d0108bfdcd2c5e4917cc08
Emilia Zambrano <emilia.zambrano@devoli.com>
2024-05-23 11:50:12 +1200

    Add TNAS Devoli Toll Free

--- scriptPorting.js
+++ scriptPorting.js
@@ -1,101 +1,122 @@
-function convertToPort(input, addSip, port, cp) {
-    // Split the input based on either a comma or a space
-    const itemsArray = input.split(/[, \n]+/);
+function convertToPort(input, addSip, port, cp, tnas) {
+  // Split the input based on either a comma or a space
+  const itemsArray = input.split(/[, \n]+/);
 
-    // Create an empty string to store the formatted table
-    let tableRows = '';
+  // Create an empty string to store the formatted table
+  let tableRows = "";
 
-    // Loop through the itemsArray and format each item
-    itemsArray.forEach((item) => {
-        // Remove leading and trailing whitespaces
-        const trimmedItem = item.trim();
+  itemsArray.forEach((item) => {
+    // Remove leading and trailing whitespaces
+    const trimmedItem = item.trim();
 
-        // Replace numbers starting with 61 or 64 with 0
-        const replacedItem = trimmedItem.startsWith('61') || trimmedItem.startsWith('64')
-            ? '0' + trimmedItem.slice(2)
-            : trimmedItem;
+    // Replace numbers starting with 61 or 64 with 0
+    const replacedItem =
+      trimmedItem.startsWith("61") || trimmedItem.startsWith("64")
+        ? "0" + trimmedItem.slice(2)
+        : trimmedItem;
 
-        // Separate the first two characters from the rest with a space
-        const formattedItem = replacedItem.slice(0, 2) + ' ' + replacedItem.slice(2);
-
-        // Add a new row to the table with the formatted item as the content of the cell
-        tableRows += `<tr><td>${formattedItem}</td></tr>`;
-    });
-	
-    // Update the item count
-    const itemCount = itemsArray.length;
-
-	// Work out title
-	let sipLine = addSip ? 'Sipline and ' : '';;
-
-    let title;
-    if (port) {
-        title = `${itemCount} x ${sipLine}DDI`;
+    // Separate the first two characters from the rest with a space
+    if (tnas) {
+      const formattedItem =
+        replacedItem.slice(0, 4) + " " + replacedItem.slice(4);
+      tableRows += `<tr><td>${formattedItem}</td></tr>`;
     } else {
-        title = `${itemCount} x Cease billing`;
+      const formattedItem =
+        replacedItem.slice(0, 2) + " " + replacedItem.slice(2);
+      tableRows += `<tr><td>${formattedItem}</td></tr>`;
     }
-	
-	// Add title
-	tableRows = `<tr><td>${title}</td></tr><tr><td>&nbsp;</td></tr>` + tableRows;
-	
-	// Init date
-	const date = new Date();
-	
-	// Add porting fee & date
-    let fee;
-    if (port) {
-        fee = `${itemCount} x Porting Fee`;
-    } else if (addSip && cp) {
-        fee = `${itemCount} x Outport Fees to CP SOM xxxxxx`;
-    } else if (addSip && !cp) {
-        fee = `${itemCount} x Outport Fees to VF SOM xxxxxx`;
-    } else {    
-        fee = 'No Outport Fee';
-    }
+  });
 
-	tableRows += `<tr><td>&nbsp;</td></tr><tr><td>${fee}<td></tr>`;
-	tableRows += '<tr><td>' + date.getDate() + '/' + String(date.getMonth() + 1).padStart(2, '0') + '</td></tr>';
+  // Update the item count
+  const itemCount = itemsArray.length;
 
-    return { tableRows, itemCount };
+  // Work out title
+  let sipLine = addSip ? "Sipline and " : "";
+
+  let title;
+  if (port) {
+    title = `${itemCount} x ${sipLine}DDI`;
+  } else if (tnas) {
+    title = `${itemCount} x TNAS Devoli Toll Free`;
+  } else {
+    title = `${itemCount} x Cease billing`;
+  }
+
+  // Add title
+  tableRows = `<tr><td>${title}</td></tr><tr><td>&nbsp;</td></tr>` + tableRows;
+
+  // Init date
+  const date = new Date();
+
+  // Add porting fee & date
+  let fee;
+  if (port || tnas) {
+    fee = `${itemCount} x Porting Fee`;
+  } else if (addSip && cp) {
+    fee = `${itemCount} x Outport Fees to CP SOM xxxxxx`;
+  } else if (addSip && !cp) {
+    fee = `${itemCount} x Outport Fees to VF SOM xxxxxx`;
+  } else {
+    fee = "No Outport Fee";
+  }
+
+  tableRows += `<tr><td>&nbsp;</td></tr><tr><td>${fee}<td></tr>`;
+  tableRows +=
+    "<tr><td>" +
+    date.getDate() +
+    "/" +
+    String(date.getMonth() + 1).padStart(2, "0") +
+    "</td></tr>";
+
+  return { tableRows, itemCount };
 }
 
-function formatPort(addSip, port = false, cp = false) {
-    const inputPortList = document.getElementById('portList').value;
-    const { tableRows, itemCount } = convertToPort(inputPortList, addSip, port, cp);
-    const formattedTableElement = document.getElementById('formattedPort');
-    const counterElement = document.getElementById('counter');
+function formatPort(addSip, port = false, cp = false, tnas = false) {
+  const inputPortList = document.getElementById("portList").value;
+  console.log(addSip, port, cp, tnas);
+  const { tableRows, itemCount } = convertToPort(
+    inputPortList,
+    addSip,
+    port,
+    cp,
+    tnas
+  );
+  const formattedTableElement = document.getElementById("formattedPort");
+  const counterElement = document.getElementById("counter");
 
-    // Update the formatted table in the HTML
-    formattedTableElement.innerHTML = tableRows;
+  // Update the formatted table in the HTML
+  formattedTableElement.innerHTML = tableRows;
 
-    // Update the item count in the HTML
-    counterElement.textContent = `Total items: ${itemCount}`;
-	
-    // Get the formatted list element
-    var formattedPortDiv = document.getElementById("formattedPort");
+  // Update the item count in the HTML
+  counterElement.textContent = `Total items: ${itemCount}`;
 
-    // Create a range to select the div content
-    var range = document.createRange();
-    range.selectNode(formattedPortDiv);
+  // Get the formatted list element
+  var formattedPortDiv = document.getElementById("formattedPort");
 
-    // Select the range
-    window.getSelection().removeAllRanges();
-    window.getSelection().addRange(range);
+  // Create a range to select the div content
+  var range = document.createRange();
+  range.selectNode(formattedPortDiv);
 
-    // Copy the selected text
-    try {
-        document.execCommand("copy");
-        //alert("List copied to clipboard!");
-    } catch (err) {
-        alert("Unable to copy the list. Your browser may not support this feature.");
-    }
+  // Select the range
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(range);
 
-    // Clear the selection
-    window.getSelection().removeAllRanges();
+  // Copy the selected text
+  try {
+    document.execCommand("copy");
+    //alert("List copied to clipboard!");
+  } catch (err) {
+    alert(
+      "Unable to copy the list. Your browser may not support this feature."
+    );
+  }
+
+  // Clear the selection
+  window.getSelection().removeAllRanges();
 }
 
 const textarea = document.getElementById("portList");
 
-textarea.addEventListener("focus", function() {
+textarea.addEventListener("focus", function () {
   textarea.value = ""; // Clear the textarea content
 });
